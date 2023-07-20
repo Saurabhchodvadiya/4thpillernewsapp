d1c80f9bbbafec9f94acb701b4358867bb9b863b
Saurabhchodvadiya <chodvadiyasaurabh@gmail.com>
2023-07-20 14:28:48 +0530

    add code

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 echo "Build start"
-python 3.9 -m  pip install -r requirements.txt
+python 3.9  -m  pip install -r requirements.txt
 python 3.9 manage.py collectstatic  --noinput --clear
 echo "Build End"
