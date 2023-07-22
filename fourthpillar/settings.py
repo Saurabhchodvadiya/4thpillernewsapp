55948003e87d59be993e6bec83557cf6ed8ce39d
Saurabhchodvadiya <chodvadiyasaurabh@gmail.com>
2023-07-22 14:31:30 +0530

    add code

--- fourthpillar/settings.py
+++ fourthpillar/settings.py
@@ -78,13 +78,22 @@ WSGI_APPLICATION = 'fourthpillar.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': '4thpiller',
+        'USER': 'root',
+        'PASSWORD': 'Admin@1234',
+        'HOST': 'localhost',
+        'PORT': 3306,  
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
